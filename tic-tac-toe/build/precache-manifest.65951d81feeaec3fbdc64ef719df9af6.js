9a0b692122a4f19b9692564f51e41a8ebfdc4f7f
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-05-18 16:43:00 +0300

    tic tac toe

---
+++ tic-tac-toe/build/precache-manifest.65951d81feeaec3fbdc64ef719df9af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3f1ac70484f3c543d1b6c66b0138bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33843c06514a6b7891e8",
+    "url": "/static/css/main.06990fef.chunk.css"
+  },
+  {
+    "revision": "75cb28f74c3c195e5a1b",
+    "url": "/static/js/2.9ee35cd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ee35cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33843c06514a6b7891e8",
+    "url": "/static/js/main.28786a6d.chunk.js"
+  },
+  {
+    "revision": "2893c4269a291b1fc016",
+    "url": "/static/js/runtime-main.5c177eb3.js"
+  }
+]);
