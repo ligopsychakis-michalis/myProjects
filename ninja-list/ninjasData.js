488d15454e9f599b241f38114c698d017f313cb2
ligopsychakis-michalis <57640815+ligopsychakis-michalis@users.noreply.github.com>
2021-12-23 22:33:28 +0200

    add ninja data

---
+++ ninja-list/ninjasData.js
@@ -0,0 +1,44 @@
+export const ninjasData = [
+    {
+        id: 0,
+        name: 'Dark Thief',
+        email: 'darkthief@ninjas.com',
+        website: 'https://darkthief.com',
+        address: { city: 'Darkish'}
+    },
+    {
+        id: 1,
+        name: 'Pink Lady',
+        email: 'pinklady@ninjas.com',
+        website: 'https://pinklady.com',
+        address: { city: 'Pinkerland'}
+    },
+    {
+        id: 2,
+        name: 'Last Breath',
+        email: 'lastbreath@ninjas.com',
+        website: 'https://lastbreath.com',
+        address: { city: 'Completion'}
+    },
+    {
+        id: 3,
+        name: 'Top Killer',
+        email: 'topkiller@ninjas.com',
+        website: 'https://topkiller.com',
+        address: { city: 'Killingness'}
+    },
+    {
+        id: 4,
+        name: 'Most Wanted',
+        email: 'mostwanted@ninjas.com',
+        website: 'https://mostwanted.com',
+        address: { city: 'Hideland'}
+    },
+    {
+        id: 5,
+        name: 'Shadow Light',
+        email: 'shadowlight@ninjas.com',
+        website: 'https://shadowlight.com',
+        address: { city: 'Shadowland'}
+    }
+];
